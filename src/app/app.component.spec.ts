68671a199b8ec04ea9d9a92ea2d67e598374f4d5
Yuvaraj Jonna <yuvaraj.j@itp.objectfrontier.com>
2019-09-09 13:56:28 +0530

    Re-Branding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo-ui'`, () => {
+  it(`should have as title 'TechCrez-UI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demo-ui');
+    expect(app.title).toEqual('TechCrez-UI');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demo-ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TechCrez-UI!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-ui';
+  title = 'TechCrez-UI';
 }
